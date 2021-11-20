beebc09937ca6372dde859e0e6bb54b6cb1db5f9
Petr Svirák <petr.svirak@gmail.com>
2021-11-20 17:24:52 +0100

    Fix link visuals and move data attribute depency out of JS
    Links did not change mouse cursor on hovering after the last change and they
    were also hard to find since they were not underlined.
    While HTML defined data attributes, JS needed to be kept in sync with that so,
    their names were made the only current function's parameters and index now
    keeps everything data-attribute-name-related within itself.

--- main.v1.1.js
+++ main.v1.1.js
@@ -1,16 +1,20 @@
-function doTheRedirect() {
-   var composer = document.getElementsByClassName("email-composer")[0];
-   var data = composer && [
-		composer.getAttribute("data-username"),
-		"@",
-		composer.getAttribute("data-firstname"),
-		composer.getAttribute("data-surename"),
-		".",
-		composer.getAttribute("data-domain")];
+function doTheRedirect(dataName1, dataName2, dataName3, dataName4) {
+	if(!dataName1 || !dataName2 || !dataName3 || !dataName4) {
+		   return false;
+	}
 
-   if(data && data.length) {
+	var composer = document.getElementsByClassName("email-composer")[0];
+	var data = composer && [
+		composer.getAttribute("data-" + dataName1),
+		"@",
+		composer.getAttribute("data-" + dataName2),
+		composer.getAttribute("data-" + dataName3),
+		".",
+		composer.getAttribute("data-" + dataName4)];
+
+	if(data && data.length) {
 	  window.open("mailto:" + data.join(""));
-   }
-   
-   return false;
+	}
+
+	return false;
 }
